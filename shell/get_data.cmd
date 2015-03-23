a06676ccda5590b5c1e34d19564351d3d31f546d
Yongxin Wang <fefe.wyx@gmail.com>
2015-03-23 10:55:56 +0800

    add some test files and script

---
+++ shell/get_data.cmd
@@ -0,0 +1,1 @@
+..\debug\GetAsmSyms.exe ..\GenNames\Debug\GenNames.asm > ..\_test\sym_names.txt
---
+++ shell/run_test.cmd
@@ -0,0 +1,2 @@
+..\bin\debug\DeMangleVC.exe ..\data\test_suit.txt > ..\_test\test_out.txt
+..\debug\UnDecorateSymbolName.exe ..\data\test_suit.txt > ..\_test\test_u.txt
